4d62b3e5b60e74b75c6125323e692d2f23d28fe0
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-11-29 12:48:45 +0700

    Fixed data bulk transfer

--- client.py
+++ client.py
@@ -18,22 +18,23 @@ class Client(KeyPair):
         self.socket.connect((HOST, PORT))
 
     def client_hello(self):
-        self.client_random = os.urandom(32)  # 32 Bytes
+        self.client_random = os.urandom(32).hex()  # 32 Bytes
         self.client_ephemeral_private_key = X25519PrivateKey.generate()
         self.client_ephemeral_public_key = self.client_ephemeral_private_key.public_key()
-        self.protocol_version = bytes(1)
-        self.cipher_suits = 'X25519'.encode()
-        self.server_name = 'example-server'.encode()
+        eph_public_key_bytes = self.client_ephemeral_public_key.public_bytes_raw().hex()
 
-        eph_public_key_bytes = self.client_ephemeral_public_key.public_bytes_raw()
+        self.protocol_version = 1
+        self.cipher_suits = 'X25519'
+        self.server_name = 'example-server'
 
-        # self.client_hello_msg = {
-        #     "ClientRandom".encode(): self.client_random,
-        #     "Version".encode(): self.protocol_version,
-        #     "EphPubKey".encode(): eph_public_key_bytes,
-        #     "Suite".encode(): self.cipher_suits,
-        #     "ServerName".encode(): self.server_name
-        # }
+        self.client_hello_msg = json.dumps({
+            "ClientRandom": self.client_random,
+            "Version": self.protocol_version,
+            "EphPubKey": eph_public_key_bytes,
+            "Suite": self.cipher_suits,
+            "ServerName": self.server_name
+        }).encode()
+
         self.socket.send(self.client_hello_msg)
 
     def communicate(self):
